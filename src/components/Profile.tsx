3785901e97d9e0bd6afef8315046bc35bbe5cd9a
Jean Diego <jeean.james@gmail.com>
2021-02-27 19:10:56 -0300

    ✨ Countdown improvements

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,9 +3,9 @@ import styles from '../styles/components/Profile.module.css'
 export function Profile() {
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/diego3g.png" alt="Diego Fernandes"/>
+      <img src="https://github.com/jeandiego.png" alt="Diego Fernandes"/>
       <div>
-        <strong>Diego Fernandes</strong>
+        <strong>Jean Diego</strong>
         <p>
         <img src="icons/level.svg" alt="Icone de Level"/>
         Level 1</p>
